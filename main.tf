140333fa310ce6edbf26691c94e7343b8f12a9c0
preetha12345 <42036837+preetha12345@users.noreply.github.com>
2024-09-11 14:40:21 +0530

    Update main.tf
    I

--- main.tf
+++ main.tf
@@ -18,3 +18,23 @@ resource "aws_dynamodb_table" "tfc_example_table" {
     type = "S"
   }
 }
+def versions = [
+    "D:\\Apps\\Java\\jdk17.0",
+    "D:\\Apps\\Java\\jdk-11.0.21",
+    "D:\\Apps\\Java\\jdk17.0.0_401"
+]
+
+def regex = ~/jdk(?:-(\d+)(?:\.(\d+)(?:\.(\d+))?)?)?(?:_(u(\d+)))?/
+
+versions.each { versionString ->
+    def matcher = versionString =~ regex
+    if (matcher) {
+        matcher.each { match ->
+            def major = match[1] ?: ''
+            def minor = match[2] ?: ''
+            def patch = match[3] ?: ''
+            def build = match[4] ?: ''
+            println "Version: ${major}.${minor}${minor ? '.' : ''}${patch}${build ? 'u' + build : ''}"
+        }
+    }
+}
